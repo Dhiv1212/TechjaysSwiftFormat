5443cd4e71224d41c3279d359f4f1f5861d8b34f
Dhivya Subramanian <dhivya.s@techjays.com>
2023-01-20 14:37:37 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TechjaysSwiftFormat
+
+A description of this package.
---
+++ Sources/TechjaysSwiftFormat/TechjaysSwiftFormat.swift
@@ -0,0 +1,6 @@
+public struct TechjaysSwiftFormat {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
